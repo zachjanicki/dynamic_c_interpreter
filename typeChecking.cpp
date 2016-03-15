aa8e1cae8ee4699a306cfa5ab32d9120bf74cca5
Zach Janicki <zjanicki88@gmail.com>
2016-03-15 14:52:08 -0400

    typeChecking.cpp

---
+++ typeChecking.cpp
@@ -0,0 +1,11 @@
+// Some type checking functions...
+
+#include <iostream>
+#include <regex>
+#include "token.h"
+
+bool isInt(token);
+bool isFloat(token);
+bool isOperator(token);
+bool isVariable(token);
+bool isKeyword(token);
