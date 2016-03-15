b9d7a960a68daddd639e0b20b18154050e1791ed
Zach Janicki <zjanicki88@gmail.com>
2016-03-15 15:05:16 -0400

    some isType functions

--- typeChecking.cpp
+++ typeChecking.cpp
@@ -2,10 +2,54 @@
 
 #include <iostream>
 #include <regex>
+#include <string>
 #include "token.h"
 
-bool isInt(token);
-bool isFloat(token);
-bool isOperator(token);
-bool isVariable(token);
-bool isKeyword(token);
+using namespace std;
+
+bool isInt(Token);
+bool isFloat(Token);
+bool isOperator(Token);
+bool isVariable(Token);
+bool isKeyword(Token);
+
+bool isInt(Token token) {
+    string tokenString = token.getVal();
+    regex regularExpression("^[+-]?(\d)+$|^\d+$");
+    if (regex_match(tokenString, regularExpression)) {
+        return true;
+    }
+    return false;
+}
+
+bool isFloat(Token token) {
+    string tokenString = token.getVal();
+    regex regularExpression("^[-+]?([0-9]*\.[0-9]+$|[0-9]+\.[0-9]*$");
+    if (regex_match(tokenString, regularExpression)) {
+        return true;
+    }
+    return false;
+}
+
+bool isOperator(Token token) {
+    string tokenString = token.getVal();
+    regex regularExpression("^[+-/*<>]{1}$|^(<=)$|^(>=)$|^(eq)$");
+    if (regex_match(tokenString, regularExpression)) {
+        return true;
+    }
+    return false;
+}
+
+bool isVariable(Token token) {
+    string tokenString = token.getVal();
+    regex regularExpression("^[a-zA-Z_]+$");
+    if (regex_match(tokenString, regularExpression)) {
+        return true;
+    }
+    return false;
+}
+
+bool isKeyword(Token, token) {
+    string tokenString = token.getVal();
+    // if tokenString is in the keyword dictionary...
+}
